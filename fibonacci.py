8a62cedaa7e6caf56787e9a6af64aa0782538c17
lincolnfiser <lincfiser07@gmail.com>
2025-10-22 11:59:53 -0400

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,23 +1,24 @@
-#!/usr/bin/env python3
+# Function 1: get a valid positive integer from the user
 def get_terms():
-    
-while True:
-    user_input = input("Enter the number of terms: ")
-    num = int(user_input)
-    if num > 0
-      return num
-    print("Please enter a positive integer.")
-    
-    # Step 2: Validate input
-    def fibonacci(n):
+    while True:
+        user_input = input("Enter the number of terms: ")
+        if user_input.isdigit():
+            num = int(user_input)
+            if num > 0:
+                return num
+        print("Please enter a positive integer.")
+
+# Function 2: generate (and print) Fibonacci numbers using a for loop
+def fibonacci(n):
     a = 0
     b = 1
-    for i in range(n):
+    for _ in range(n):
         print(a)
         temp = a + b
         a = b
         b = temp
 
+# Function 3: print the sequence (drives the program)
 def main():
     terms = get_terms()
     print("Fibonacci sequence:")
@@ -25,5 +26,4 @@ def main():
 
 # Run the program
 main()
-# Fibonacci Sequence Exercise with functions
-# TODO: (Read detailed instructions in the Readme file)
+
